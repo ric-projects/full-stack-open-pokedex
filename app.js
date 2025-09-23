be63c8ffa3b6540a75d44098661cbbae1b81f90b
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-09-23 15:20:57 -0700

    Implementing health check

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req,res) => {
-  res.send('2')
+  res.send('3, testing the health check')
 })
 
 app.get('/health', (_req, res) => {
