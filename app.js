6e01b4d37e4a54a8c4a9720d708920e7fed20364
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-09-23 15:41:34 -0700

    Restore to previous state with a working health check. Exercise 11.12 completed

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req,res) => {
-  res.send('4, broken health check')
+  res.send('5')
 })
 
 app.get('/health', (_req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if(true) throw('error...')
   res.send('ok')
 })
 
