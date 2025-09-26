ad7cae462cd65e7423e4908502dd3699892ac12c
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-09-26 15:42:00 -0700

    Test the deploy pipeline

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req,res) => {
-  res.send('11, adding version tags')
+  res.send('12, adding version tags')
 })
 
 app.get('/health', (_req, res) => {
